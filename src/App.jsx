d213fa2a7ae25ed40db75a092508f344e47a3fd9
rakib <mdsakilhasan@gmail.com>
2023-04-12 16:07:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <div >
+      <h1 className='font-bold'>Raki</h1>
+      <p>hello</p>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  )
+}
+
+export default App
